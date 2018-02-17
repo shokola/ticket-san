fe470d65ac8766bb6bda26ecd519b2a54679bb06
kseta <keigo.seta@gmail.com>
2018-02-17 18:08:16 +0900

    チケットのバリデーションで空文字を弾く・ステータスの値をチェック

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,5 @@
 class Ticket < ApplicationRecord
+  validates :title, presence: true
+  validates :status, presence: true, inclusion: ['todo', 'doing', 'done']
+  validates :assignee, presence: true
 end
