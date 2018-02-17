15a86251eb88c502c386d5a47d5dd8a6bc823a59
SETA Keigou <keigo.seta@gmail.com>
2018-02-17 18:19:00 +0900

    Update ticket.rb

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,4 @@
 class Ticket < ApplicationRecord
   validates :title, presence: true
   validates :status, presence: true, inclusion: ['todo', 'doing', 'done']
-  validates :assignee, presence: true
 end
