ae212b178faa2a25d859c9417cfc4ec3368c9d5c
kseta <keigo.seta@gmail.com>
2018-02-17 17:56:35 +0900

    トップページのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :tickets, only: [:new, :create, :show, :edit, :update]
+
+  root to: 'tickets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
