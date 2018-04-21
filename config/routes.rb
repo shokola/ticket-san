a0e2f98dec452bfcc9401df7318e8692d55a75dd
suginoy <suginoyasuhiro@gmail.com>
2018-04-21 15:26:26 +0900

    Add each status routing eg. /tickets/todo
    以下のようなルーティングになる。
    コントローラがトップレベルになり、サブリソースとしてふさわしくない。
    変更する。
    
    ```
         Prefix Verb  URI Pattern                 Controller#Action
     todo_index GET   /tickets/todo(.:format)     todo#index
    doing_index GET   /tickets/doing(.:format)    doing#index
     done_index GET   /tickets/done(.:format)     done#index
        tickets POST  /tickets(.:format)          tickets#create
     new_ticket GET   /tickets/new(.:format)      tickets#new
    edit_ticket GET   /tickets/:id/edit(.:format) tickets#edit
         ticket GET   /tickets/:id(.:format)      tickets#show
                PATCH /tickets/:id(.:format)      tickets#update
                PUT   /tickets/:id(.:format)      tickets#update
           root GET   /                           tickets#index
    ```

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :tickets, only: [:new, :create, :show, :edit, :update]
+  resources :tickets, only: [:new, :create, :show, :edit, :update] do
+    collection do
+      resources :todo, only: :index
+      resources :doing, only: :index
+      resources :done, only: :index
+    end
+  end
 
   root to: 'tickets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
