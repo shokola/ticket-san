2130f4d07f29579ebb7d1ea48cc3e8bbb8f9bcd9
suginoy <suginoyasuhiro@gmail.com>
2018-04-21 15:56:44 +0900

    Fix RoutingError `GET /tickets/todo`
    `GET /tickets/todo` が
    `Tickets::TodoController#index` ではなく、うっかり
    `TicketsController#show` にマッチしてしまっていた。

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :tickets, only: [:new, :create, :show, :edit, :update]
   namespace :tickets do
     resources :todo, only: :index
     resources :doing, only: :index
     resources :done, only: :index
   end
+  resources :tickets, only: [:new, :create, :show, :edit, :update]
 
   root to: 'tickets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
